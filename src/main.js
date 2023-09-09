282bfe29efa8f2e516ad876817df9a2532a04cd1
ZacariasBongiovanni <zacariasbongiovanni@gmail.com>
2023-09-09 01:55:06 -0300

    Flujo

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import UI from "./scenes/UI";
 import Preload from "./scenes/Preload";
 import Camaras from "./scenes/Cameras";
 import Menu from "./scenes/Menu";
+import Game from "./scenes/Game";
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 640,
+  height: 480,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
@@ -29,7 +30,7 @@ const config = {
       debug: false,
     },
   },
-  scene: [HelloWorldScene, UI, Preload,Camaras, Menu],
+  scene: [Preload, HelloWorldScene, UI,Camaras, Menu, Game],
 };
 
 export default new Phaser.Game(config);
--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -6,5 +6,20 @@ export default class Preload extends Phaser.Scene {
       super("preload");
     }
 
+   
+
+    preload(){
+        this.load.image("bMenu","assets/background/bmenu.jpg");
+        this.load.image("room","assets/background/bhabitacion.jpg");
+        this.load.spritesheet("player","assets/sprites/astronautasheet.png",{
+            frameWidth: 125,
+            frameHeight: 280,  
+        } )
+    }
+
+    create(){
+        this.scene.start("menu")
+    }
+
     
 }
