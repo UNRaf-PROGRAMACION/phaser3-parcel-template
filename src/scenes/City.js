fbe4333ffc56bdb7fd0dbc182e477eb7ba7b602f
unknown <iniguezagustin2021@gmail.com>
2023-09-19 13:44:58 -0300

    Added background, attack, colision y mainmenu

--- src/scenes/City.js
+++ src/scenes/City.js
@@ -1,74 +1,74 @@
-//    import Phaser from "phaser";
-//     import events from "./EventCenter";
-//     import Player from "../components/Player";
+    import Phaser from "phaser";
+     //import events from "./EventCenter";
+     import Player from "../components/Player";
   
 
-//    // Main biome, player starts the game here and after completing some tasks unlocks the desert
-//    // Has pathway to forest
-//    // holds some secret collectibles
-//    // Can access bossArena
-//    // Resistence camp with npcs are here
-//    // Has normal enemies
-//    // save station
-//   export default class City extends Phaser.Scene {
-//       constructor() {
-//         super("City");
-//         this.level;
-//         this.vida;
-//         this.experience;
-//         this.player;
-//         this.velocityPlayer;
-//       }
+    //  Main biome, player starts the game here and after completing some tasks unlocks the desert
+    //  Has pathway to forest
+    //  holds some secret collectibles
+    //  Can access bossArena
+    //  Resistence camp with npcs are here
+    //  Has normal enemies
+    //  save station
+   export default class City extends Phaser.Scene {
+       constructor() {
+         super("City");
+         this.level;
+         this.vida;
+         this.experience;
+         this.player;
+         this.velocityPlayer;
+       }
 
-//        init(data){
-//           this.level= data.level || 1
-//           this.vida= data.vida || 3
-//           this.experience= data.experience || 0
-//           this.velocityPlayer= data.velocityPlayer || 200
+        init(data){
+           this.level= data.level || 1
+           this.vida= data.vida || 3
+           this.experience= data.experience || 0
+           this.velocityPlayer= data.velocityPlayer || 200
 
-//    }
+    }
 
-//        create(){
+        create(){
         
-//          const map = this.make.tilemap({ key: "City" });
-//          const layerbackGround = map.addTilesetImage("TDJ2 - tileset", "Mapcity");
-//          const background = map.createLayer("Ground", layerbackGround, 0, 0);
+          const map = this.make.tilemap({ key: "City" });
+          const layerbackGround = map.addTilesetImage("TDJ2 - tileset", "Mapcity");
+          const background = map.createLayer("Ground", layerbackGround, 0, 0);
          
-//       const layerObstacle = map.addTilesetImage(
-//        "TDJ2 - tileset","Mapcity",
+       const layerObstacle = map.addTilesetImage(
+        "TDJ2 - tileset","Mapcity",
       
-//       );
-//       const Obstacle = map.createLayer(
-//         "Deco",
-//         layerObstacle,
-//         0,
-//         0
+       );
+       const Obstacle = map.createLayer(
+         "Deco",
+         layerObstacle,
+         0,
+         0
        
-//       );
-//       this.player= new Player (
-//        this,
-//        212,
-//        200,
-//        "C4",
-//        this.velocityPlayer
+       );
+       this.player= new Player (
+        this,
+        212,
+        200,
+        "C4",
+        this.velocityPlayer
      
 
 
-//    );
+    );
      
 
-//       Obstacle.setCollisionByProperty({ colision: true });
-//       Obstacle.setSize(50,50)
-//        this.cameras.main.startFollow(this.player);
-//        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-//        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-//       this.physics.add.collider(this.player,Obstacle);
+       Obstacle.setCollisionByProperty({ colision: true });
+       Obstacle.setSize(50,50)
+        this.cameras.main.startFollow(this.player);
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+       this.physics.add.collider(this.player,Obstacle);
      
-//        }
+        }
 
-//        update(){
-//           this.player.update();
+        update(){
+           this.player.update();
 
-//        }
+        }
 
-//    }
+    }
--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,36 +1,36 @@
-//  import Phaser from "phaser";
-//  import events from "./EventCenter";
-// //  starts the game
-// //  Continue game
-// //  credits
-// //  Language selector
-//  export default class MainMenu extends Phaser.Scene {
-//    constructor() {
-//      super("MainMenu");
-//    }
+ import Phaser from "phaser";
+ //import events from "./EventCenter";
+//  starts the game
+//  Continue game
+//  credits
+//  Language selector
+ export default class MainMenu extends Phaser.Scene {
+   constructor() {
+     super("MainMenu");
+   }
 
-//    create() {
+   create() {
 
-//      const canvasWidth = this.sys.game.config.width;
-//      const canvasHeight = this.sys.game.config.height;
+     const canvasWidth = this.sys.game.config.width;
+     const canvasHeight = this.sys.game.config.height;
 
-//      const bgImage = this.add.image(400, 300, "menuBg");
+     const bgImage = this.add.image(400, 300, "menuBg");
 
-//      bgImage.setScale(canvasWidth / bgImage.width, canvasHeight / bgImage.height);
-//      bgImage.setPosition(canvasWidth / 2, canvasHeight / 2);
+     bgImage.setScale(canvasWidth / bgImage.width, canvasHeight / bgImage.height);
+     bgImage.setPosition(canvasWidth / 2, canvasHeight / 2);
 
 
-//      this.add.image(990, 300, "title").setScale(2);
-//      let startButton = this.add.text(850, 500, "Jugar", {
-//        fontSize: "128px",
-//        fontFamily: "impact",
-//        fill: "#FFFFFF"
-//      }).setInteractive();
+     this.add.image(990, 300, "title").setScale(2);
+     let startButton = this.add.text(850, 500, "Jugar", {
+       fontSize: "128px",
+       fontFamily: "impact",
+       fill: "#FFFFFF"
+     }).setInteractive();
 
-//      startButton.on("pointerdown", () => {
-//          this.scene.start("City");
-//      });
-//    }
+     startButton.on("pointerdown", () => {
+         this.scene.start("City");
+     });
+   }
 
-//    update() {}
-//  }
+   update() {}
+ }
