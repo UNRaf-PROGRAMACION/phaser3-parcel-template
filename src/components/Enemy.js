893604df2da30d5dff13f14d887bd1de65691a19
Sebastian Faetani <128256756+Sebastian-Faetani@users.noreply.github.com>
2023-09-19 20:38:13 -0300

    attack kinda added

--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -1,9 +1,9 @@
 import Phaser from "phaser";
 
-export default class Enemy extends Phaser.GameObjects.Sprite {
+export default class Enemies extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y, texture, velocity) {
     super(scene, x, y, texture);
-    this.setTexture("Enemy");
+    this.setTexture("Enemies")
     scene.add.existing(this);
     scene.physics.add.existing(this);
     this.body.allowGravity = false;
